9d85223916e661c333c870d33bbc810382c26bbb
infinity-atom <Leonard.jj@outlook.com>
2023-10-26 14:48:19 +1000

    Add assets and TODO page parser

--- src/endpoints/assets.js
+++ src/endpoints/assets.js
@@ -1,7 +1,16 @@
+const path = require("path");
+const fs = require("fs");
+
 module.exports = {
-    endpoint_path: "/asset/:assetid",
+    endpoint_path: "/assets/:assetid",
     endpoint_type: "GET",
     endpoint_run: function(req, res) {
-        
+        if(fs.existsSync(path.join(__dirname, "../wiki/assets/" + req.params.assetid))) {
+            res.status(200);
+            res.sendFile(path.join(__dirname, "../wiki/assets/" + req.params.assetid));
+        } else {
+            res.status(404);
+            res.send("File Not Found");
+        }
     }
 }
--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
-const app = require("express")();
+const express = require("express");
+const app = express();
 const fs = require("fs");
 const path = require("path");
 
